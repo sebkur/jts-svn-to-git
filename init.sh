faca7e58b4582178359e4a757e0a6ca51b35ad63
Sebastian Kürten <sebastian@topobyte.de>
2016-01-02 14:59:44 +0100

    Removed --revisions argument from scripts

--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@ CONVERT="$DIR/convert.to.unix.sh"
 mkdir -p "$REPO"
 cd "$REPO"
 
-svn2git svn://svn.code.sf.net/p/jts-topo-suite/code --authors ../jts.authors --revision 1:5
+svn2git svn://svn.code.sf.net/p/jts-topo-suite/code --authors ../jts.authors
 git filter-branch -f --tree-filter "$CONVERT" -- --all
 
 cat ../REPO_CONFIG >> .git/config
--- update.sh
+++ update.sh
@@ -7,5 +7,5 @@ CONVERT="$DIR/convert.to.unix.sh"
 
 cd "$REPO"
 
-svn2git svn://svn.code.sf.net/p/jts-topo-suite/code --authors ../jts.authors --revision 1:9
+svn2git svn://svn.code.sf.net/p/jts-topo-suite/code --authors ../jts.authors
 git filter-branch -f --tree-filter "$CONVERT" svn/trunk
